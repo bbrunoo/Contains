94b9b0eebbb4dfa4454ddbc80fe9bde147f87fa9
bbrunoo <brunoedsanntos@gmail.com>
2021-11-28 00:19:02 -0300

    contain

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f = "Aula de programação";
+            string p = "de";
+            WriteLine($" A frase '{f}' contem '{p}' ? ");
+            WriteLine($"--------------------------------------------");
+
+            if(f.Contains("de") == true)
+            {
+                WriteLine($"Sim, a frase contem '{p}'");
+            }
+
+            else
+            {
+                WriteLine($"Não, a frase não contem '{p}'");
+            }
+        }
+    }
+}
